48c6a117bd46f4d954abfa5ca2c70bceb4af4051
Shaiju Raju <shaijur2014@gmail.com>
2025-12-04 11:49:43 +0530

    initial commit

---
+++ emojipedia/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div>
+      <div id='Heading'>
+        <h1>
+          <span>Emojipedia</span>
+        </h1>
+      </div>
+
+      <div>
+        
+      </div>
+    </div>
+      
+    </>
+  )
+}
+
+export default App
